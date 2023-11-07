1cea387cc1e50645d94851689571a87d5d1ae569
christianmb70 <christianmb70@gmail.com>
2023-11-06 19:12:05 -0800

    “storage”

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "Uploads": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
