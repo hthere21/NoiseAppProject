b5389b44ca1674f2a097c1f743759fdf93fb8e5c
christianmb70 <christianmb70@gmail.com>
2023-11-07 09:53:41 -0800

    “aws”

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,35 @@ import 'home_page.dart'; // Import the home_page.dart file
 import 'data_page.dart';
 // import 'settings_page.dart';
 
-void main() {
-  runApp(MyApp());
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:amplify_storage_s3/amplify_storage_s3.dart';
+import 'amplifyconfiguration.dart';
+
+Future<void> _configureAmplify() async {
+  try {
+    final auth = AmplifyAuthCognito();
+    final storage = AmplifyStorageS3();
+    await Amplify.addPlugins([auth, storage]);
+
+    // call Amplify.configure to use the initialized categories in your app
+    await Amplify.configure(amplifyconfig);
+  } on Exception catch (e) {
+    safePrint('An error occurred configuring Amplify: $e');
+  }
 }
 
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await _configureAmplify();
+  runApp(MyApp());
+}
+// void main() {
+//   runApp(MyApp());
+// }
+
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
